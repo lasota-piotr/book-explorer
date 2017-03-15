8ea10ce4b42cc09ee205f02a9413359478738142
Piotr Lasota <lasota.piotr1@gmail.com>
2017-03-15 20:51:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Book explorer
+
+This project is part of Udemy course: The Full JavaScript & ES6 Tutorial - (including ES7 & React)
+
+### Features:
+* Searching books in Google Play 
+
+### Used:
+* React
+* Webpack
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: ['./app/index.js'],
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    port: 3001,
+    contentBase: './build',
+    inline: true
+  }
+}
