81a315f7cd6b844beee6314f54bbf1d6451b68bc
Piotr Lasota <lasota.piotr1@gmail.com>
2017-03-15 21:50:57 +0100

    added react

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
 import 'babel-polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Global from './components/Global';
+
+ReactDOM.render(
+  <Global />, document.getElementById('root')
+);
