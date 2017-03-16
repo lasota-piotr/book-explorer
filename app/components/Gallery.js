df1ca629d0b29d67befbdfb16f231a5788b7fe3d
Piotr Lasota <lasota.piotr1@gmail.com>
2017-03-16 12:42:42 +0100

    added searching books feature

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -2,8 +2,32 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
   render() {
+    const alternate = 'http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/book-icon.png';
+
     return (
-      <div>Gallery Component</div>
+      <div className="c-gallery">
+        {
+          this.props.items.map((item, index) => {
+            let {title, imageLinks, infoLink} = item.volumeInfo;
+            return (
+              <a
+                key={index} className='c-book'
+                href={infoLink}
+                target="_blank"
+              >
+                <img
+                  src={imageLinks !== undefined ? imageLinks.thumbnail : alternate}
+                  alt="book image"
+                  className="c-book__img"
+                />
+                <div className="c-book__body">
+                  <h3 className="c-book__title">{title}</h3>
+                </div>
+              </a>
+            )
+          })
+        }
+      </div>
     )
   }
 }
